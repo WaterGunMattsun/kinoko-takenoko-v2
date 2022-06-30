ca47feac463ccf8fc70c680bdd853cde4ead6f58
MatsukawaTakumi <s1f102201908@iniad.org>
2022-06-30 16:47:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ kinoko_count = 3
 takenoko_count = 5
 messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
 
+
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
+
 
 @app.route('/vote', methods=['POST'])
 def answer():
@@ -21,7 +23,7 @@ def answer():
     messages.append(request.form.get("message"))
     if len(messages) > 3:
         messages = messages[-3:]
-    
+
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
 
@@ -33,10 +35,13 @@ def answer():
         message = re.sub(r'>', r'&gt;', message)
         message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
         message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
+        message = re.sub(
+            r"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?", r"<a herf='\0'>\0</a>", message)
         message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
             message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
 
     return render_template('vote.html', **vars())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
